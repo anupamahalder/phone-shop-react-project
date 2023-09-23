e1878de54f6820eaed8830ec9e9efa6f3aeeb166
Anupama_Halder <anupamahalder2022@gmail.com>
2023-09-23 20:28:52 +0530

    create grid layout for phones card

--- src/Components/Phones/Phones.jsx
+++ src/Components/Phones/Phones.jsx
@@ -3,9 +3,9 @@ import PhoneCard from './PhoneCard';
 const Phones = ({phones}) => {
     console.log(phones);
     return (
-        <div>
-            <h1 className="text-2xl text-gray-600 font-bold text-center py-7">All categories phones</h1>
-            <div>
+        <div className='bg-gray-100 py-10 mx-auto'>
+            <h1 className="text-2xl text-gray-900 font-bold text-center pb-7">All categories phones</h1>
+            <div className='mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 px-4'>
                 {/* Loop through the the phones array  */}
                 {
                     phones?.map(phone => <PhoneCard key={phone.id} phone={phone}></PhoneCard>)
