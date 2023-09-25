66accf4e3064ca52dad96e7607c9758207b9666b
Anupama_Halder <anupamahalder2022@gmail.com>
2023-09-25 12:38:02 +0530

    Update two states,noFound and setFavorites while removing data

--- src/Pages/Favorites/Favorites.jsx
+++ src/Pages/Favorites/Favorites.jsx
@@ -23,29 +23,35 @@ const Favorites = () => {
     const handleRemoveFromFavorite = () =>{
         //clear local stoarge
         localStorage.clear();
+        //and setiing favorites item as empty array to show no products item in the screen
+        setFavorites([]);
+        //also want to see the no product found on screen
+        setNoFound('No product is found!');
     }
     return (
         <>
         {/* if the length of favorites are greater than 0 then show a delete btn */}
+        <div className="h-screen">
         <div>
             {favorites.length > 0 && <button onClick={handleRemoveFromFavorite}
-            className="block w-40 mx-auto my-5 select-none rounded-lg bg-[#ec4899] py-3 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] text-white disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            className="block w-40 mx-auto my-5 select-none rounded-lg bg-pink-500 py-3 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] text-white disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
             type="button">Remove All Products</button>}
         </div>
-        <div className="bg-gray-50 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="bg-gray-50">
             {/* getting data from local storage  */}
             {/* Now conditional rendering will be if data found then show favorites if not found then show no data found  */}
             {
-                noFound ? <p className="h-[60vh] flex justify-center items-center text-center p-10 font-semibold text-xl">{noFound}</p>: 
+                noFound ? <p className="h-[70vh] flex justify-center items-center text-center p-10 font-semibold text-xl">{noFound}</p>: 
                 <div>
                     {
-                        <div>
+                        <div className="h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                             {/* map through the products that are saved in local storage  */}
                             {favorites.map(phone => <FavoriteCard key={phone.id} phone={phone}></FavoriteCard>)}
                         </div>
                     }
                 </div>
             }
+        </div>
         </div>
         </>
     );
