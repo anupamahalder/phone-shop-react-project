b86ad235997d1846a50ca1a0ab4e089a66f1009a
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-01 20:02:00 +0530

    Getting value from input field by user input through login and register form

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,36 +1,45 @@
 import { Link } from "react-router-dom";
 const Login = () => {
+    //function to handle login
+    const handleLogin = e =>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+    }
     return (
         <div className="bg-base-200">
-            <div className="hero min-h-screen w-3/4 mx-auto">
+            <div className="hero py-10 w-3/4 mx-auto">
                 <div className="hero-content flex flex-col-reverse">
                     <div className="text-center lg:text-bottom">
                         <p>Do not have account? 
-                        <Link to='/register' className=" text-[#db2777]"> Register</Link></p>
+                        <Link to='/register' className=" text-[#db2777] font-semibold"> Register</Link></p>
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
                         <div className="flex justify-center items-center">
                             <h1 className="text-3xl bg- font-bold text-[#db2777]">Login now</h1>
                         </div>
-                        <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input type="text" placeholder="email" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Password</span>
-                        </label>
-                        <input type="text" placeholder="password" className="input input-bordered" />
-                        <label className="label">
-                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                        </label>
-                        </div>
-                        <div className="form-control mt-6">
-                        <button className="py-3 font-semibold rounded-xl bg-[#db2777] text-white hover:bg-pink-700">Login</button>
-                        </div>
+                        <form onSubmit={handleLogin}>
+                            <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                            </div>
+                            <div className="form-control mt-6">
+                            <button className="py-3 font-semibold rounded-xl bg-[#db2777] text-white hover:bg-pink-700">Login</button>
+                            </div>
+                        </form>
                     </div>
                     </div>
                 </div>
--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,36 +1,46 @@
 import { Link } from "react-router-dom";
 const Register = () => {
+    //function handleRegister
+    const handleRegister = e =>{
+        e.preventDefault();//to avoid reloading
+        console.log('form submitted!');
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+    }
     return (
         <div className="bg-base-200">
-            <div className="hero min-h-screen w-3/4 mx-auto">
+            <div className="hero py-10 w-3/4 mx-auto">
                 <div className="hero-content flex flex-col-reverse">
                     <div className="text-center lg:text-bottom">
                         <p>Already have account? 
-                        <Link to='/login' className=" text-[#db2777]"> Login</Link></p>
+                        <Link to='/login' className=" text-[#db2777] font-semibold"> Login</Link></p>
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
                         <div className="flex justify-center items-center">
                             <h1 className="text-3xl bg- font-bold text-[#db2777]">Register now</h1>
                         </div>
-                        <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input type="text" placeholder="email" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Password</span>
-                        </label>
-                        <input type="text" placeholder="password" className="input input-bordered" />
-                        <label className="label">
-                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                        </label>
-                        </div>
-                        <div className="form-control mt-6">
-                        <button className="py-3 font-semibold rounded-xl bg-[#db2777] text-white hover:bg-pink-700">Register</button>
-                        </div>
+                        <form onSubmit={handleRegister}>
+                            <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                            </div>
+                            <div className="form-control mt-6">
+                            <button className="py-3 font-semibold rounded-xl bg-[#db2777] text-white hover:bg-pink-700">Register</button>
+                            </div>
+                        </form>
                     </div>
                     </div>
                 </div>
