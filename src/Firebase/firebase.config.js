497d4bb7fb43295bbb5636f80d80754b614b288d
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-02 12:01:00 +0530

    Added login form and registration form and handle validation

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+import {getAuth} from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// export default app;
+//we can create auth and send it to other places
+const auth = getAuth(app);
+export default auth;
--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,12 +1,49 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { Link } from "react-router-dom";
+import auth from "../Firebase/firebase.config";
+import { useState } from "react";
+import { AiOutlineEye,AiOutlineEyeInvisible } from "react-icons/ai";
 const Register = () => {
+    //if there is any error like email already is in use
+    const [registerError, setRegisterError] = useState('');
+    //declare state to show success message
+    const [registerSuccess, setRegisterSuccess] = useState('');
+    //declare a state to toggle between show and hide password by default hidden 
+    const [showPassword, setShowPassword] = useState(false);
     //function handleRegister
     const handleRegister = e =>{
         e.preventDefault();//to avoid reloading
         console.log('form submitted!');
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email, password);
+        // console.log(email, password);
+        //reset error
+        setRegisterError('');
+        //reset success
+        setRegisterSuccess('');
+
+        //by default password is string so we can validate that given string is less than 6 or not
+        if(password.length < 6){
+            setRegisterError("Password should be at least 6 characters!");
+            return;
+        }
+        else if(!/[A-Z]/.test(password)){
+            setRegisterError('Password should have atleast one UPPER CASE character!');
+            return;
+        }
+        //create user, it a promise so then and catch
+        createUserWithEmailAndPassword(auth,email,password)
+        // then means success
+        .then(result =>{
+            const newUser = result.user;
+            setRegisterSuccess('You have registered successfully!')
+            console.log(newUser);
+        })
+        .catch(error =>{
+            console.log(error);
+            //set error
+            setRegisterError(error.message);
+        })
     }
     return (
         <div className="bg-base-200">
@@ -26,13 +63,18 @@ const Register = () => {
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" name="email" placeholder="email" className="input input-bordered" />
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required/>
                             </div>
                             <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                            <input type={showPassword?'text':'password'} name="password" placeholder="password" className="input input-bordered" required/> 
+                            {
+                                showPassword ?
+                            <AiOutlineEye onClick={()=>setShowPassword(!showPassword)} className="absolute cursor-pointer right-11 mt-12"></AiOutlineEye>:
+                            <AiOutlineEyeInvisible onClick={()=>setShowPassword(!showPassword)} className="absolute cursor-pointer right-11 mt-12"></AiOutlineEyeInvisible>
+                            }
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
@@ -41,6 +83,14 @@ const Register = () => {
                             <button className="py-3 font-semibold rounded-xl bg-[#db2777] text-white hover:bg-pink-700">Register</button>
                             </div>
                         </form>
+                        {/* showing error if any error occured */}
+                        {
+                            registerError && <p className="text-red-500 pt-2">{registerError}</p>
+                        }
+                        {/* showing success message  */}
+                        {
+                            registerSuccess && <p className="text-green-600 pt-2">{registerSuccess}</p>
+                        }
                     </div>
                     </div>
                 </div>
