bb23099d3a45c25488853fedc6b8098c7a4e7936
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-08 12:09:30 +0530

    Added an observer to get info is user is null or not

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,7 +1,7 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import auth from "../Firebase/firebase.config";
 import PropTypes from 'prop-types';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 // create context 
 export const AuthContext = createContext(null);
 
@@ -20,7 +20,21 @@ const AuthProvider = ({children}) => {
         return signInWithEmailAndPassword(auth, email,password);
     }
     
-    
+    //set an observer 
+    useEffect(()=>{
+        // if state changed then it will take auth and a callback function and put it into a variable
+        // unSubscribe takes an reference and get a callback function by which we can detached relation 
+        const unSubscribe = onAuthStateChanged(auth, currentUser =>{
+            // set user to state if user not there then it will become null 
+            setUser(currentUser);
+            console.log('observing current user inside useEffect of Authprovider ',currentUser);
+        })
+
+        // return with an arrow function by calling unSubscribe function 
+        return () =>{
+            unSubscribe();
+        }
+    },[]);
 
 
     //share user info with other component
